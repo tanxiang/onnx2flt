82162cd57ecfe55745050ca701e21a34dffc1333
ttand <wqazsedc@gmail.com>
2022-09-28 14:51:28 +0800

    new file:   fbs/gnt.fbs 	modified:   main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,32 @@
 
 #include <iostream>
+#include <fstream>
+#include <onnx/onnx_pb.h>
+#include <string>
+#include <map>
+#include <sstream>
 
-int main(int argc,char* argv[]){
+void usage(const std::string &filename)
+{
+    std::cout << "Usage: " << filename
+              << " onnx_model output_filename [table_file]" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3 && argc != 4)
+    {
+        usage(argv[0]);
+        return -1;
+    }
+    const std::string table_file = argc == 4 ? argv[3] : "";
+
+    onnx::ModelProto model_proto;
+    std::ifstream ifs(argv[1], std::ios::in | std::ios::binary);
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    // FIXME: Handle the return value
+    model_proto.ParseFromString(ss.str());
+    std::cout<<"model_proto.ir_version = "<<model_proto.ir_version()<<std::endl;
     return 0;
 }
