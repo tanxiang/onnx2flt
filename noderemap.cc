c3e959e0a234e53f9d2712d67f7fef9e21bb06d1
ttand <wqazsedc@gmail.com>
2022-10-08 14:31:16 +0800

    check ks

--- noderemap.cc
+++ noderemap.cc
@@ -1,22 +1,25 @@
 #include "noderemap.hh"
 
 OpToReMap::OpToReMap() {
-     emplace("Dropout", [](flatbuffers::FlatBufferBuilder &flatbuffers,
-                              const onnx::NodeProto &node, mapContext &context)
-       { //auto flNode = getFlNode<nn::Dropout>(flatbuffers, node, context);
-          //return UnionPair(flNode);
-        });
+  emplace("Dropout",
+          [](flatbuffers::FlatBufferBuilder &flatbuffers,
+             const onnx::NodeProto &node,
+             mapContext &
+                 context) { // auto flNode = getFlNode<nn::Dropout>(flatbuffers,
+                            // node, context); return UnionPair(flNode);
+          });
 
-        emplace("Shape", [](flatbuffers::FlatBufferBuilder &flatbuffers,
-                            const onnx::NodeProto &node, mapContext &context) {
-          //auto flNode = getFlNode<nn::>(flatbuffers, node, context);
-          //return UnionPair(flNode);
-        });
+  emplace("Shape", [](flatbuffers::FlatBufferBuilder &flatbuffers,
+                      const onnx::NodeProto &node, mapContext &context) {
+    // auto flNode = getFlNode<nn::>(flatbuffers, node, context);
+    // return UnionPair(flNode);
+  });
 
-
-        emplace("Constant", [](flatbuffers::FlatBufferBuilder &flatbuffers,
-                               const onnx::NodeProto &node, mapContext &context)
-       { //auto flNode = getFlNode<nn::Cons>(flatbuffers, node, context); return
-       //UnionPair(flNode);
-        });
-  }
+  emplace("Constant", [](flatbuffers::FlatBufferBuilder &flatbuffers,
+                         const onnx::NodeProto &node,
+                         mapContext &context) { // auto flNode =
+                                                // getFlNode<nn::Cons>(flatbuffers,
+                                                // node, context); return
+    // UnionPair(flNode);
+  });
+}
